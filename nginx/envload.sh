4ad19fef1646282cf793d028cee530388fb554cc
Rull Deef <deeroll666@gmail.com>
2022-10-16 14:35:58 +0300

    update configs

--- nginx/envload.sh
+++
@@ -1,6 +0,0 @@
-# load ip addresses to environment
-
-export API_SERVER_IP=$(getent hosts server | awk '{ print $1 }')
-export PGADMIN_IP=$(getent hosts pgadmin | awk '{ print $1 }')
-
-envsubst '$API_SERVER_IP $PGADMIN_IP' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
